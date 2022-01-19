d1fb3d84e78d09a823f39e0ac68cba70e1123142
naiarazoroza <93262292+naiarazoroza@users.noreply.github.com>
2022-01-19 14:14:23 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (input.buttonIsPressed(Button.B)) {
+        basic.showString("e")
+        basic.showString("" + (receivedNumber))
+    }
+})
 input.onButtonPressed(Button.A, function () {
     basic.showString("i")
     basic.showString("" + (tenplnt))
-})
-input.onButtonPressed(Button.B, function () {
-    let tenpExt = 0
-    basic.showString("e")
-    basic.showString("" + (tenpExt))
 })
 let tenplnt = 0
 radio.setGroup(4)
